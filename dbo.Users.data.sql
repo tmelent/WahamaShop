5909caa7187d89c83a5a7f14839bef8d3850454c
Timofey Melentev <50305166+tmelent@users.noreply.github.com>
2021-01-04 15:32:52 +0300

    Delete dbo.Users.data.sql

--- dbo.Users.data.sql
+++
@@ -1,5 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Users] ON
-INSERT INTO [dbo].[Users] ([Id], [Login], [Password], [RoleId], [FirstName], [LastName], [PhoneNumber], [ExpirationDate]) VALUES (5, N'5f791509-71c1-4bd2-bdaa-71710e9db194', N'/kZl83yop6sQn0FGiQ2tVl76tyy9i8yxHhQeA2t8d/gIV1bp', 2, N'Temporary', N'Temporary', N'+70000000', N'2019-09-25 14:46:20')
-INSERT INTO [dbo].[Users] ([Id], [Login], [Password], [RoleId], [FirstName], [LastName], [PhoneNumber], [ExpirationDate]) VALUES (6, N'ins4n3.owo@com', N'uaR+E8dl4sSotCtPSwX1NkCyhTvNjlT5N9InRwS2cYR29QFY', 2, N'Тимофей', N'Мелентьев', N'+79257212682', NULL)
-INSERT INTO [dbo].[Users] ([Id], [Login], [Password], [RoleId], [FirstName], [LastName], [PhoneNumber], [ExpirationDate]) VALUES (7, N'melentino.lol@yandex.ru', N'z33A1SGixWp+Ic/1YY9MG9m9Oxt2DL1hcQ3ObwQCsH9uOP5M', 2, N'Владимир', N'Суздальский', N'+7925725122', NULL)
-SET IDENTITY_INSERT [dbo].[Users] OFF
